570e6df6e772e67a44515ffdeb6e4e7238d094cb
fouzaan <fouzaanahmed1@gmail.com>
2022-08-02 08:25:53 +0530

    commit0.01

---
+++ src/Screens/Home/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomeStyle.css";
+function HomeScreen() {
+  return (
+    <div className="HomeMain">
+      <div className="Title">
+        <h1 className="TitleText">Helow</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
