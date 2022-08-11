84340288f98a09b6135c639c8c551c8e70166499
fouzaan <fouzaanahmed1@gmail.com>
2022-08-11 16:27:04 +0530

    commit0.1.3

--- src/Screens/Blog/Blog.js
+++ src/Screens/Blog/Blog.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import "./Blog.css";
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className="blog_container">
+      <img
+        className="imgWaiting"
+        src={require("./Waiting-bro.png")}
+        alt="me coding illustration"
+      ></img>
+      <h1 className="SubTitleText"> Coming Soon!</h1>
+    </div>
+  );
 }
 
 export default Blog;
--- src/Screens/InstaPosts/InstaPosts.js
+++ src/Screens/InstaPosts/InstaPosts.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function InstaPosts() {
-  return <div>InstaPosts</div>;
+  return (
+    <div className="blog_container">
+      <img
+        className="imgWaiting"
+        src={require("./Waiting-bro.png")}
+        alt="me coding illustration"
+      ></img>
+      <h1 className="SubTitleText"> Coming Soon!</h1>
+    </div>
+  );
 }
 
 export default InstaPosts;
